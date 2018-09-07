c01209f14d38c8b61b25acb5d76916fc33aeb4cb
Jarrett Briody <jarrettbriody@gmail.com>
2018-09-07 18:22:21 -0400

    e02 complete

---
+++ E02 - VS17 GLFW Configuration/Main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <iostream>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+#include "GLM\glm.hpp"
+#include "OpenGL-Tutorials\shader.hpp"
+
+#endif
