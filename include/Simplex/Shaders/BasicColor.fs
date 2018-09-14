ab15361704f030c844410d43f49ba0e77f8b1ac1
jab1089 <jab1089@IGMOH1380-12>
2018-09-14 10:52:22 -0400

    not yet working

--- include/Simplex/Shaders/BasicColor.fs
+++ include/Simplex/Shaders/BasicColor.fs
@@ -8,6 +8,8 @@ out vec4 Fragment;
 
 void main()
 {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+		Color = glm::vec3(1.0f - Color.r, 1.0f - Color.g, 1.0f - Color.b);
 	Fragment = vec4(Color,1);
 	
 	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
