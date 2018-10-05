89c1d35a0df79d788b4355aa94c6a031c25b5d4b
Jarrett Briody <jarrettbriody@gmail.com>
2018-10-05 00:25:06 -0400

    somehow the basic color shader got overwritten after having done the assignment, the working shader was originally put in the dropbox which proves i have done it before the due date, im updating this now to fix the problem with the repo

--- include/Simplex/Shaders/BasicColor.fs
+++ include/Simplex/Shaders/BasicColor.fs
@@ -8,15 +8,10 @@ out vec4 Fragment;
 
 void main()
 {
-	//Color = vec3(1.0f - Color.r, 1.0f - Color.g, 1.0f - Color.b);
-	Fragment = vec4(Color,1);
-	//Fragment = vec4(1.0f - Color.r, 1.0f - Color.g, 1.0f - Color.b,1);
+	Fragment = vec4(1.0f - Color.r, 1.0f - Color.g, 1.0f - Color.b,1);
 	
-	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0){
-		//SolidColor = vec3(1.0f - SolidColor.r, 1.0f - SolidColor.g, 1.0f - SolidColor.b);
-		Fragment = vec4(SolidColor, 1);
-		//Fragment = vec4(1.0f - SolidColor.r, 1.0f - SolidColor.g, 1.0f - SolidColor.b, 1);
-	}
+	if(SolidColor.r != -1.0 && SolidColor.g != -1.0 && SolidColor.b != -1.0)
+		Fragment = vec4(1.0f - SolidColor.r, 1.0f - SolidColor.g, 1.0f - SolidColor.b, 1);
 		
 	return;
 }
